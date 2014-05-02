8523f1ff3b9e59c31c5f8af6ef130068dd72e487
Mathias <mathias.bourgoin@gmail.com>
2014-05-02 17:19:17 +0200

    test 1

--- {{cookiecutter.repo_name}}/web.ml
+++
@@ -1,9 +0,0 @@
-let js s = Js.string s
-
-let _ =
-  let main =
-    Js.Opt.get
-      (Dom_html.document##getElementById (js"main"))
-      (fun () -> assert false)
-  in
-  main##innerHTML <- js"Hello, world!"
